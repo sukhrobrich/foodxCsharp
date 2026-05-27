10b5b92f79e370cc4a15fa263f0e4fc5870c1ddd
Suxrob G'aybullayev <suxrobrich9262@gmail.com>
2026-05-27 19:35:45 +0000

    update

--- WindowsFormsApp1/WindowsFormsApp1/WaiterPage.cs
+++ WindowsFormsApp1/WindowsFormsApp1/WaiterPage.cs
@@ -12,7 +12,7 @@ namespace WindowsFormsApp1
 {
     public class WaiterPage : Form
     {
-	private test1;
+	
         private FlowLayoutPanel flpTables;
         private Label lblWelcome;
         private Timer refreshTimer;
